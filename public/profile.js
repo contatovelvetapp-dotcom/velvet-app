66065be13616e991372a35ad215ba4c7104430ff
sandrasamantanutri-glitch <sandrasamanta.nutri@gmail.com>
2026-01-13 19:20:41 +0000

    Revert "."
    This reverts commit 15f1a6e89b1f222bad1f9c8ea252e30fcf601243.

--- public/profile.js
+++ public/profile.js
@@ -2,23 +2,17 @@
 // AUTH GUARD
 // ===============================
 const role  = localStorage.getItem("role");
-const token = localStorage.getItem("token");
 const stripe = Stripe("pk_live_51SlJ2zJb9evIocfiAuPn5wzOJqWqn4e356uasq214hRTPsdQGawPec3iIcD43ufhBvjQYMLKmKRMKnjwmC88iIT1006lA5XqGE");
 let elements;
 window.__CLIENTE_VIP__ = false;
 const socket = io();
 
 // autentica socket
-if (token) {
-  socket.emit("auth", { token });
-}
+socket.emit("auth", { token });
 
 // registra cliente online
-if (role === "cliente" && token) {
-  const decoded = decodeJWT(token);
-  if (decoded?.id) {
-    socket.emit("loginCliente", Number(decoded.id));
-  }
+if (role === "cliente") {
+  socket.emit("loginCliente", Number(decodeJWT(token).id));
 }
 
 function decodeJWT(token) {
